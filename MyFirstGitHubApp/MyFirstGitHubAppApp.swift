8d4c5855636cd07f91feb939f63154b8279cabc3
Priyanshu kumar singh <155616952+Priyanshu8905@users.noreply.github.com>
2026-01-19 10:37:51 +0530

    Initial commit

---
+++ MyFirstGitHubApp/MyFirstGitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstGitHubAppApp.swift
+//  MyFirstGitHubApp
+//
+//  Created by priyanshu kumar singh on 19/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstGitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
